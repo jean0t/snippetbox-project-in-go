f9cd27755748d5811c35a8f7654dde54b5268a5e
jean0t <joaomouryf@hotmail.com>
2024-03-18 18:18:26 -0300

    Logs are now sent to StdOut and StdError

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Config struct {
@@ -12,11 +13,15 @@ type Config struct {
 }
 
 func main() {
-
+	// get the config variables
 	cfg := new(Config)
 	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.StaticDir, "static-dir", "./cmd/ui/static/", "Path to static assets")
 	flag.Parse()
+
+	// personalized log to help identify errors from normal log messages
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ltime|log.Ldate)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ltime|log.Ldate|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -24,10 +29,16 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting Server on %s\n", cfg.Addr)
-	err := http.ListenAndServe(cfg.Addr, mux)
-	log.Fatal(err)
+	// creating a new server to set a different route for Log
+	srv := &http.Server{
+		Addr:     cfg.Addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting Server on %s\n", cfg.Addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
