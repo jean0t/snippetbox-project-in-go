f6bf2a32b1befff3d881814a8882bddcfbe02a57
jean0t <joaomouryf@hotmail.com>
2024-03-18 18:04:06 -0300

    cli commands and hard code refactorated in variables to help read the code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type Config struct {
+	Addr      string
+	StaticDir string
+}
+
 func main() {
+
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./cmd/ui/static/", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./cmd/ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting Server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting Server on %s\n", cfg.Addr)
+	err := http.ListenAndServe(cfg.Addr, mux)
 	log.Fatal(err)
 }
